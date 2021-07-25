281cc4525a9d457106ab8fc049a493ef129609d4
Shashank <shashankjyrt@gmail.com>
2021-07-25 18:26:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let openingPage = document.querySelector(".opening--page");
+let mainHeader = document.querySelector(".main--header");
+let hamburger = document.querySelector(".hamburger");
+let navBackground = document.querySelector(".hamburger--background");
+let mobileNavItem = document.querySelectorAll(
+  ".hamburger__mobile-navbar ul li a"
+);
+let checkBox = document.querySelector(".hamburger-checkBox");
+openingPage.addEventListener("click", () => {
+  openingPage.classList.add("hide-my-page");
+  mainHeader.classList.add("header-down");
+
+  if (hamburger) {
+    hamburger.classList.add("hamburger__open");
+  }
+  if (navBackground) {
+    navBackground.classList.add("hamburger__open");
+  }
+});
+mobileNavItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    checkBox.click();
+  });
+});
